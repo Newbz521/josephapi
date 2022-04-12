ef9d491ba064767a5ea7d2f7a18c8e80d9aaafae
Lawrence Yee <lawrenceyee521@lawrences-mbp.myfiosgateway.com>
2022-04-11 23:26:06 -0400

    done

--- seed/itineraries.js
+++ seed/itineraries.js
@@ -20,4 +20,32 @@ export const itineraries = [
     timeOfStay: 31,
     img: "https://a.cdn-hotels.com/gdcs/production108/d1460/1e7b3f6f-b054-4b27-9c05-3ee963b6b8a5.jpg",
   },
+  {
+    city: "Japan",
+    arrival: "January 1st",
+    departure: "February 1st",
+    timeOfStay: 31,
+    img: "https://www.commonwealthfund.org/sites/default/files/styles/countries_hero_desktop/public/country_image_Japan.jpg",
+  },
+  {
+    city: "Korea",
+    arrival: "January 1st",
+    departure: "February 1st",
+    timeOfStay: 31,
+    img: "https://discovery.cathaypacific.com/wp-content/uploads/2021/02/HERO-Seoul-South-Korea-skyline-Bongeunsa-Temple-2000-1266.jpg",
+  },
+  {
+    city: "Rio",
+    arrival: "January 1st",
+    departure: "February 1st",
+    timeOfStay: 31,
+    img: "https://a.cdn-hotels.com/gdcs/production143/d357/42fb6908-dcd5-4edb-9f8c-76208494af80.jpg",
+  },
+  {
+    city: "London",
+    arrival: "January 1st",
+    departure: "February 1st",
+    timeOfStay: 31,
+    img: "http://babyloncitytours.com/wp-content/uploads/2016/09/london-441853_960_720.jpg",
+  },
 ];
